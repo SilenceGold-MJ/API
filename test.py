0bd8e9a727acce370b4fc321236b0daea5168bf4
SilenceGold-MJ <397135766@qq.com>
2020-08-27 17:06:34 +0800

    删除不用的py

--- test.py
+++
@@ -1,9 +0,0 @@
-from framework.GetExceltolist import GetExceltolist
-from framework.Method import Merhod
-
-
-for data in GetExceltolist().GetExceltolist():
-    print(data)
-    PM = Merhod().merhod(data["url"], data["method"], data["param"], data["headers"], data["testname"])
-
-
