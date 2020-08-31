c240ce69473d7c23ea576a78d7cf6012fff61c4e
SilenceGold-MJ <397135766@qq.com>
2020-08-31 17:12:39 +0800

    兼容有完整请求地址和无完整地址找配置文件拼接

--- framework/RExcetodicl.py
+++ framework/RExcetodicl.py
@@ -1,9 +1,11 @@
 from openpyxl import load_workbook
 from framework.logger import Logger
+import re
 import configparser,os
 proDir = os.getcwd()
 configPath = os.path.join(proDir, "config\config.ini")
 cf = configparser.ConfigParser()
+
 cf.read(configPath,encoding="utf-8-sig")
 #print(cf.get("Data", "1")
 logger = Logger(logger="RWExcel").getlog()
@@ -17,9 +19,15 @@ class RExcetodicl():
         sheet1 = wb[sheet_names[0]]  # 打开第一个 sheet 工作表
         # 获取C列的所有数据
         datas=[]
+        pattern = re.compile(u'^((https|http|ftp|rtsp|mms)?:\/\/)[^\s]+')
         for b, e, c, a, d,f,g,h in zip(sheet1["B"][1:], sheet1["E"][1:], sheet1["C"][1:], sheet1["A"][1:],sheet1["D"][1:], sheet1["F"][1:],sheet1["G"][1:],sheet1["H"][1:]):
+
+            if (pattern.search(c.value))==None:#判断读取的地址是否有前缀地址http://192.168.1.153:8562
+                url='%s%s'%(cf.get("Data", "address"),c.value)#无前缀读取配置文件添加前缀
+            else:
+                url=c.value#有前缀使用读取的完整地址
             data={
-                "url":'%s%s'%(cf.get("Data", "address"),c.value),
+                "url":url,
                 "param": f.value,
                 "method": d.value,
                 "headers": e.value,
